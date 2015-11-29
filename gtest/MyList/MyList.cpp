4f28e3b315d37d4fcc3a64b0af31bb837bf74379
Zhorin Sergey <ser4038772@mail.ru>
2015-11-29 15:18:23 +0300

    Evil Incarnate

--- gtest/MyList/MyList.cpp
+++ gtest/MyList/MyList.cpp
@@ -3,6 +3,7 @@
 #include "iostream"
 
 using namespace std;
+
 CNode* CreateList(int count, const int* vals) {
   if (count <= 0) return 0;
   CNode *head = new CNode;
@@ -39,4 +40,59 @@ void print(CNode *r) {
     r = r->next;
   }
   cout << endl;
+}
+
+void sort(CNode **a) {
+  if (*a == 0) {
+    throw exception("Empty node");
+  }
+  if ((*a)->next == 0) {
+    return;
+  }
+  if ((*a)->next->next == 0) {
+    if ((*a)->val > (*a)->next->val) {
+      CNode *tmp = (*a)->next;
+      (*a)->next->next = (*a);
+	  (*a)->next = 0;
+	  (*a) = tmp;
+	}
+    return;
+  }
+  CNode *curr;
+  int flag = 0;
+  while (flag == 0) {
+    flag = 1;
+	curr = (*a);
+	if (curr->val > curr->next->val) {
+	  flag = 0; 
+	  CNode *second_element = (*a)->next;
+	  CNode *third_element = (*a)->next->next;
+	  (*a)->next->next = (*a);
+	  (*a)->next = third_element;
+	  (*a) = second_element;
+	}
+	while (curr->next->next != 0) {
+	  if (curr->next->val > curr->next->next->val) {
+	    CNode *first_element, *second_element, *third_element;
+		flag = 0;
+		first_element = curr->next; 
+		second_element = curr->next->next; 
+		third_element = curr->next->next->next;
+		second_element->next = first_element;
+		first_element->next = third_element;
+		curr->next = second_element;
+	  }
+	  curr = curr->next;
+	}
+  }
+}
+
+bool comp(CNode *a, CNode *b) {
+  while (a->next != 0) {
+	if (a->val != b->val)
+	  return false;
+	a = a->next;
+	b = b->next;
+  }
+  return true;
 }
